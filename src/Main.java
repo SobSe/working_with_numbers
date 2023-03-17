5f0201c42a6b74d2840d9d50bfa4fabf74873b93
Sobolev Sergey <shutle@gmail.com>
2023-03-17 12:04:09 +0300

    Change sorting

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Set<Integer> result = new TreeSet<>();
+        List<Integer> result = new ArrayList<>();
         for (Integer element : list) {
             if (element <= 0 ) continue;
             if (element % 2 != 0) continue;
             result.add(element);
         }
+        Collections.sort(result);
         for (Integer integer : result) {
             System.out.println(integer);
         }
